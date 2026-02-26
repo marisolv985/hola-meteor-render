a6f6856f6c3acc7329ae2203e391ed6bd19708e1
marisolv985 <23301249@uttt.edu.mx>
2026-02-25 19:38:45 -0600

    Fix local and production DB config

--- server/db.js
+++ server/db.js
@@ -3,14 +3,22 @@ const { Pool } = pkg;
 
 const isProduction = process.env.NODE_ENV === "production";
 
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ...(isProduction && {
-    ssl: {
-      rejectUnauthorized: false
-    }
-  })
-});
+export const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
+    : {
+        user: 'postgres',
+        host: 'localhost',
+        database: 'meteorapp',
+        password: '12345',
+        port: 5432
+      }
+);
 
 pool.query(`
   CREATE TABLE IF NOT EXISTS registros (
